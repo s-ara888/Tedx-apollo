445ce96c5a2569ae7ac59815f68a1e6797e46c6f
sara <sara.waleed.184007@gmail.com>
2025-07-26 21:35:12 +0300

    tedx

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const lyre = document.getElementById('lyre');
+  const apolloText = document.getElementById('apollo-text');
+
+  window.addEventListener('scroll', function () {
+    const scrollValue = window.scrollY;
+
+    // Cap scroll movement at 71.2
+    const maxScroll = 71.2;
+    const cappedScroll = Math.min(scrollValue, maxScroll);
+
+    // Lyre movement
+    lyre.style.top = cappedScroll * 0.9 + 'px';
+
+  });
+});
+
+
